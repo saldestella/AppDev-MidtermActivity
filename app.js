88780e1dbd557074a38f26d07d0d62210872f6be
Stella Salde <stellaasalde@gmail.com>
2024-10-06 05:27:44 +0800

    [Add] App.js Configuration

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const userRoutes = require('./routes/user');
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/user', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Server running on port ${PORT}');
+});
